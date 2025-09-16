57f94be89b30f3ef27dee5f006e3f4b9a10a4ef0
malleswar <malleswar.mca@gmail.com>
2025-09-16 19:03:23 +0530

    spring boot test jenkins added wrapper tesst

--- src/main/java/com/demo/HelloService.java
+++ src/main/java/com/demo/HelloService.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class HelloService {
     public String sayHello() {
-        return "Hello, World!";
+        return "Hello, World! failed";
     }
 }
 
